cf5ba0b37ee632ff1e9169d8b573b9b80953b451
yogesh786221 <ravindrafzb1270@gmail.com>
2025-05-07 23:12:13 +0530

    aa

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,5 +9,5 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'gaurbrothers App';
+  title = 'Hello Yogesh babu tohar projectwa run ho gaywa';
 }
